0c16eb1deff2b4bf82d5cf1902bc6500e3b09f55
MuhammadDaffa <daffaaja530@mail.com>
2024-05-15 10:42:58 +0700

    pratikum 09

---
+++ praktikum09/resources/views/nilai.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    @php
+        $nama = 'Yanwar';
+        $nilai = 95.99;
+    @endphp
+    @if ($nilai >= 60)
+        @php $ket = "Lulus"; @endphp
+    @else
+        @php $ket = "Gagal"; @endphp
+    @endif
+    <h1>Siswa {{ $nama }} dengan nilai {{ $nilai }} dinyatakan {{ $ket }}</h1>
+</body>
+</html>
---
+++ praktikum09/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/kabar', function () {
+    return view('kondisi');
+});
+
+Route::get('/nilai', function () {
+    return view('nilai');
+});
+
+Route::get('/pasien', function () {
+    return view('pasien');
+});
+
+Route::get('/admin',[AdminController::class, 'index']);

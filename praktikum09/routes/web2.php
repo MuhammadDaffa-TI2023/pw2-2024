16fa09379d7790a751c13c4f34e12bf8793691ad
MuhammadDaffa <daffaaja530@mail.com>
2024-06-10 18:47:02 +0700

    pratikum 9 and pratikum 10 and pratikum 11 and pratikum 12 and pratikum 13 and pratikum 14

--- praktikum09/routes/web2.php
+++ praktikum09/routes/web2.php
@@ -3,18 +3,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-Route::middleware('auth')->group(function () {
-    Route::get('/', function () {
+Route::get('/', function () {
     return view('welcome');
 });
 
@@ -44,16 +33,3 @@ Route::get('/admin/kelurahan/{kelurahan}', [KelurahanController::class, 'show'])
 Route::get('/admin/kelurahan/{kelurahan}/edit', [KelurahanController::class, 'edit'])->name('kelurahans.edit');
 Route::put('/admin/kelurahan/{kelurahan}', [KelurahanController::class, 'update'])->name('kelurahans.update');
 Route::delete('/admin/kelurahan/{kelurahan}', [KelurahanController::class, 'destroy'])->name('kelurahans.destroy');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
